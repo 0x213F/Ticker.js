01dc5de7a6efb0b4bd2ddf75b2be998bb4df07ff
jschlth2 <jschlth2@illinois.edu>
2017-01-08 00:52:23 -0500

    example update

--- script.js
+++ script.js
@@ -34,6 +34,6 @@ function clock_tick() {
 }
 
 clock_init();
-var counter = new Ticker("time", ['0','1','2','3','4','5','6','7','8','9'], 1666);
+var counter = new Ticker("time", ['0','1','2','3','4','5','6','7','8','9'], 750);
 clock_tick();
 var tick = setInterval(clock_tick ,1000);
